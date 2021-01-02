48042b3d90489bbde0f00e5048ce2b68a8734a80
Hamish Barjonas <msa@barjonas.com>
2021-01-01 21:03:46 -0800

    Add new Common Windows converter, control, StringItem

---
+++ Barjonas.Common.Windows/Converters/EnumerableStringToObjectsConverter.cs
@@ -0,0 +1,32 @@
+﻿// (C) Barjonas LLC 2020
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Barjonas.Common.Converters
+{
+    public class EnumerableStringToObjectsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            if (value is IEnumerable<string> strings)
+            {
+                return strings.Select(s => new ViewModel.StringItem(s));
+            }
+            
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            => Binding.DoNothing;
+    }
+}
