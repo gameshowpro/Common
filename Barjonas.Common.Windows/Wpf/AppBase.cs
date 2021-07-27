b43f5157825d5515d63b104f026a2840a49812db
Hamish Barjonas <msa@barjonas.com>
2021-07-26 22:32:20 -0400

    Remove some supressions

--- Barjonas.Common.Windows/Wpf/AppBase.cs
+++ Barjonas.Common.Windows/Wpf/AppBase.cs
@@ -91,9 +91,7 @@ namespace Barjonas.Common.Wpf
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
-        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void IComponentConnector.Connect(int connectionId, object target)
         {
             this._contentLoaded = true;
