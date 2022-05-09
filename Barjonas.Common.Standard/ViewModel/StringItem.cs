76c5dfb2e4acaf0e01d850e73b784538a54856db
Hamish Barjonas <msa@barjonas.com>
2022-05-09 14:50:10 -0700

    Move ICommand implementations to standard library to allow sharing with MAUI apps

---
+++ Barjonas.Common.Standard/ViewModel/StringItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barjonas.Common.ViewModel
+{
+    public class StringItem
+    {
+        internal StringItem(string value) => Value = value;
+
+        public string Value { get; }
+    }
+}
