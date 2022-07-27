24057955e29c5b239e7421f482f5db8daaf4ccec
Hamish Barjonas <hamish@barjonas.com>
2022-07-27 10:52:55 -0700

    Add focus behaviors

---
+++ Barjonas.Common.Windows/View/FocusOnDataContextChangedBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace Barjonas.Common.View
+{
+    /// <summary>
+    /// Behavior which will set keyboard focus to the associated object whenever its data context changes.
+    /// </summary>
+    public class FocusOnDataContextChangedBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.DataContextChanged += AssociatedObject_DataContextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.DataContextChanged -= AssociatedObject_DataContextChanged;
+        }
+
+        private void AssociatedObject_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Keyboard.Focus(AssociatedObject);
+            if (AlsoClear && AssociatedObject is TextBox tb)
+            {
+                tb.Clear();
+            }
+        }
+
+
+
+        public bool AlsoClear
+        {
+            get { return (bool)GetValue(s_alsoClearProperty); }
+            set { SetValue(s_alsoClearProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for AlsoClear.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty s_alsoClearProperty =
+            DependencyProperty.Register("AlsoClear", typeof(bool), typeof(FocusOnDataContextChangedBehavior), new PropertyMetadata(false));
+
+    }
+}
---
+++ Barjonas.Common.Windows/View/FocusOnVisibleBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace Barjonas.Common.View
+{
+    /// <summary>
+    /// Behavior which will set keyboard focus to the associated object whenever it becomes visible.
+    /// </summary>
+    public class FocusOnVisibleBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.IsVisibleChanged += AssociatedObject_IsVisibleChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.IsVisibleChanged -= AssociatedObject_IsVisibleChanged;
+        }
+
+        private void AssociatedObject_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (AssociatedObject.IsVisible)
+            {
+                Keyboard.Focus(AssociatedObject);
+            }
+        } 
+    }
+}
