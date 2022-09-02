540bab41f21e8e40e6f8d6c14af60c330bdfb76b
Hamish Barjonas <msa@barjonas.com>
2022-09-02 13:42:52 -0700

    Add array parameter support to MatchToBooleanConverter

--- Barjonas.Common.Standard/BaseConverters/MatchToBooleanConverter.cs
+++ Barjonas.Common.Standard/BaseConverters/MatchToBooleanConverter.cs
@@ -1,12 +1,14 @@
 ﻿// (C) Barjonas LLC 2018
 
 using System;
+using System.Collections;
 using System.Globalization;
+using System.Linq;
 
 namespace Barjonas.Common.BaseConverters;
 
 /// <summary>
-/// Return a visibility or boolean depending on match.
+/// Return a visibility or boolean depending on match to one or more parameters.
 /// </summary>
 public abstract class MatchToBooleanConverter : ICommonValueConverter, ICommonMultiValueConverter
 {
@@ -17,6 +19,14 @@ public abstract class MatchToBooleanConverter : ICommonValueConverter, ICommonMu
         _doNothing = doNothing;
     }
 
+    /// <summary>
+    /// Return a <see cref="bool"/> or equivilent depending on whether the first element in <paramref name="value"/> equal the second element.
+    /// </summary>
+    /// <param name="values">An array containing two values, each supplied from a binding</param>
+    /// <param name="targetType">The type to return. Either <see cref="bool"/> or equivilent visibility</param>
+    /// <param name="parameter">Ignored</param>
+    /// <param name="culture"></param>
+    /// <returns></returns>
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
         if (values[0]?.GetType().IsEnum == true && values[1]?.GetType() == typeof(int))
@@ -26,14 +36,37 @@ public abstract class MatchToBooleanConverter : ICommonValueConverter, ICommonMu
         return BooleanToType((values[0] == null && values[1] == null) || values[0]?.Equals(values[1]) == true, targetType);
     }
 
+    /// <summary>
+    /// Return a <see cref="bool"/> or equivilent depending on whether <paramref name="value"/> matches the <paramref name="parameter"/>.
+    /// If <paramref name="parameter"/> is an array, return a bool depending on whether any of its elements equal <paramref name="value"/> 
+    /// </summary>
+    /// <param name="value">The value supplied by a binding.</param>
+    /// <param name="targetType">The type to return. Either <see cref="bool"/> or equivilent visibility</param>
+    /// <param name="parameter">The value to compare to or an array of values to compare to</param>
+    /// <returns></returns>
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        var rawResult = parameter switch
+        return BooleanToType(GetRawResult(value, parameter), targetType);
+        bool GetRawResult(object value, object parameter)
+            =>
+            (parameter is IEnumerable paramList && AnyElementMatches(value, paramList)) 
+            ||
+            parameter switch
+            {
+                Type t => t.IsAssignableFrom(value?.GetType()),
+                _ => (value == null && parameter == null) || value?.Equals(parameter) == true
+            };
+        bool AnyElementMatches(object value, IEnumerable paramList)
         {
-            Type t => t.IsAssignableFrom(value?.GetType()),
-            _ => (value == null && parameter == null) || value?.Equals(parameter) == true,
-        };
-        return BooleanToType(rawResult, targetType);
+            foreach (object item in paramList)
+            {
+                if (GetRawResult(value, item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
     protected abstract object BooleanToType(bool value, Type type);
