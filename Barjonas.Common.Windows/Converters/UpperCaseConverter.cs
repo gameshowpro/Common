0d69f2c1fd96ac24f8a7b48cd0bd264d02ea9d18
Hamish Barjonas <hamish@barjonas.com>
2022-09-28 22:59:09 -0700

    Add UpperCaseConverter

---
+++ Barjonas.Common.Windows/Converters/UpperCaseConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Barjonas.Common.Converters
+{
+    public class UpperCaseConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString()?.ToUpper();
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
