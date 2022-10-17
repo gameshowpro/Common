66d96a14108c1087a3aa01274cf9ab9a11cf4f6f
Hamish Barjonas <msa@barjonas.com>
2022-10-17 14:29:33 -0700

    Add support for multiple xkeys devices

---
+++ Barjonas.Common.Windows/Model/TriggerComposite.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barjonas.Common.Model;
+
+/// <summary>
+/// A trigger which composes multiple child triggers into a single object.
+/// </summary>
+public class TriggerComposite : ITrigger
+{
+    public event EventHandler<TriggerArgs>? Triggered;
+    public TriggerComposite(IEnumerable<ITrigger> sourceTriggers)
+    {
+        foreach (ITrigger trigger in sourceTriggers)
+        {
+            trigger.Triggered += RelayTrigger;
+        }
+    }
+
+    private void RelayTrigger(object? sender, TriggerArgs args)
+        => Triggered?.Invoke(sender, args);
+}
