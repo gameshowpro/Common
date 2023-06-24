2aa10c68ca4408202171ac279b36cdad29b551d8
Hamish Barjonas <msa@barjonas.com>
2023-06-23 19:25:54 -0700

    Enhance NullToMinusOne converter

--- Barjonas.Common.Windows/Converters/NullToMinusOneConverter.cs
+++ Barjonas.Common.Windows/Converters/NullToMinusOneConverter.cs
@@ -2,9 +2,57 @@
 
 internal class NullToMinusOneConverter : IValueConverter
 {
+    public bool DisplayIsOneBased { get; set; }
     public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
-        => value ?? -1;
+    {
+        if (targetType == typeof(double))
+        {
+            if (value == null)
+            {
+                return DisplayIsOneBased ? 0d : -1d;
+            }
+            else
+            {
+                return (double)value + (DisplayIsOneBased ? 1d : 0d);
+            }
+        }
+        else
+        {
+            if (value == null)
+            {
+                return DisplayIsOneBased ? 0 : -1;
+            }
+            else
+            {
+                return (int)value + (DisplayIsOneBased ? 1 : 0);
+            }
+        }
+    }
 
     public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        => (value is int valInt && valInt == -1) || (value is double valDou && valDou == -1) ? null : value;
+    {
+        switch (value)
+        {
+            case int valInt:
+                if (DisplayIsOneBased)
+                {
+                    return valInt == 0 ? null : valInt - 1;
+                }
+                else
+                {
+                    return valInt == -1 ? null : valInt;
+                }
+            case double valDouble:
+                if (DisplayIsOneBased)
+                {
+                    return valDouble == 0d ? null : valDouble - 1d;
+                }
+                else
+                {
+                    return valDouble == -1d ? null : valDouble;
+                }
+            default:
+                return null;
+        }
+    }
 }
