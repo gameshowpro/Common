5d541ef721ded55ec270c4da8a61812f070fcbb9
Hamish Barjonas <github@barjonas.com>
2023-09-25 09:12:28 -0700

    Add ping hosts feature

---
+++ Barjonas.Common.Standard/Model/PingHostSettings.cs
@@ -0,0 +1,33 @@
+﻿namespace Barjonas.Common.Model;
+
+public class PingHostSettings : ObservableClass
+{
+    public PingHostSettings() : this(null, null)
+    { }
+
+    public PingHostSettings(string? host, string? displayName)
+    {
+        _host = host ?? string.Empty;
+        _displayName = displayName ?? string.Empty;
+    }
+
+    private string _host = "localhost";
+    [JsonProperty, DefaultValue("localhost")]
+    public string Host
+    {
+        get { return _host; }
+        set { SetProperty(ref _host, value); }
+    }
+
+    private string _displayName;
+    /// <summary>
+    /// A name which can be used shown on the UI to distinguish this device instance from another of the same type.
+    /// </summary>
+    [JsonProperty]
+    public string DisplayName
+    {
+        get { return _displayName; }
+        set { SetProperty(ref _displayName, value); }
+    }
+}
+
---
+++ Barjonas.Common.Standard/Model/PingHosts.cs
@@ -0,0 +1,11 @@
+﻿namespace Barjonas.Common.Model;
+
+public class PingHosts
+{
+    public PingHosts(IEnumerable<PingHostSettings> settings, CancellationToken cancellationToken)
+    {
+        Items = new(settings.Select(s => new PingHost(s, cancellationToken)));
+    }
+
+    public ObservableCollection<PingHost> Items { get; }
+}
