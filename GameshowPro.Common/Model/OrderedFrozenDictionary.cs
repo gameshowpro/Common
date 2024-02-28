3df9fbb1e0da159ef541ef5ac1a4c0c079507345
Hamish Barjonas <github@barjonas.com>
2024-02-28 09:48:48 -0800

    Added new ordered collection classes

---
+++ GameshowPro.Common/Model/OrderedFrozenDictionary.cs
@@ -0,0 +1,113 @@
+﻿namespace GameshowPro.Common.Model;
+
+
+//
+// Summary:
+//     Provides a set of initialization methods for instances of the GameshowPro.Common.Model.OrderedFrozenDictionary`2
+//     class, which is a wrapper around a <seealso cref="FrozenDictionary"/> that also keeps a reference to the source array without affecting the original order.
+public static class OrderedFrozenDictionary
+{
+    //
+    // Summary:
+    //     Creates a GameshowPro.Common.Model.OrderedFrozenDictionary`2 with the specified key/value
+    //     pairs.
+    //
+    // Parameters:
+    //   source:
+    //     The key/value pairs to use to populate the dictionary.
+    //
+    //   comparer:
+    //     The comparer implementation to use to compare keys for equality. If null, System.Collections.Generic.EqualityComparer`1.Default
+    //     is used.
+    //
+    // Type parameters:
+    //   TKey:
+    //     The type of the keys in the dictionary.
+    //
+    //   TValue:
+    //     The type of the values in the dictionary.
+    //
+    // Returns:
+    //     A GameshowPro.Common.Model.OrderedFrozenDictionary`2 that contains the specified keys
+    //     and values.
+    public static OrderedFrozenDictionary<TKey, TValue> ToOrderedFrozenDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source, IEqualityComparer<TKey>? comparer = null) where TKey : notnull
+        => new(source.Select(kvp => kvp.Value).ToImmutableArray(), source.ToFrozenDictionary(comparer));
+    //
+    // Summary:
+    //     Creates a GameshowPro.Common.Model.OrderedFrozenDictionary`2 from an System.Collections.Generic.IEnumerable`1
+    //     according to specified key selector function.
+    //
+    // Parameters:
+    //   source:
+    //     An System.Collections.Generic.IEnumerable`1 from which to create a GameshowPro.Common.Model.OrderedFrozenDictionary`2.
+    //
+    //
+    //   keySelector:
+    //     A function to extract a key from each element.
+    //
+    //   comparer:
+    //     An System.Collections.Generic.IEqualityComparer`1 to compare keys.
+    //
+    // Type parameters:
+    //   TSource:
+    //     The type of the elements of source.
+    //
+    //   TKey:
+    //     The type of the key returned by keySelector.
+    //
+    // Returns:
+    //     A GameshowPro.Common.Model.OrderedFrozenDictionary`2 that contains the keys and values
+    //     selected from the input sequence.
+    public static OrderedFrozenDictionary<TKey, TSource> ToOrderedFrozenDictionary<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey>? comparer = null) where TKey : notnull
+            => new(source.ToImmutableArray(), source.ToFrozenDictionary(keySelector, comparer));
+    //
+    // Summary:
+    //     Creates a GameshowPro.Common.Model.OrderedFrozenDictionary`2 from an System.Collections.Generic.IEnumerable`1
+    //     according to specified key selector and element selector functions.
+    //
+    // Parameters:
+    //   source:
+    //     An System.Collections.Generic.IEnumerable`1 from which to create a GameshowPro.Common.Model.OrderedFrozenDictionary`2.
+    //
+    //
+    //   keySelector:
+    //     A function to extract a key from each element.
+    //
+    //   elementSelector:
+    //     A transform function to produce a result element value from each element.
+    //
+    //   comparer:
+    //     An System.Collections.Generic.IEqualityComparer`1 to compare keys.
+    //
+    // Type parameters:
+    //   TSource:
+    //     The type of the elements of source.
+    //
+    //   TKey:
+    //     The type of the key returned by keySelector.
+    //
+    //   TElement:
+    //     The type of the value returned by elementSelector.
+    //
+    // Returns:
+    //     A GameshowPro.Common.Model.OrderedFrozenDictionary`2 that contains the keys and values
+    //     selected from the input sequence.
+    public static OrderedFrozenDictionary<TKey, TElement> ToOrderedFrozenDictionary<TSource, TKey, TElement>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey>? comparer = null) where TKey : notnull
+    {
+        KeyValuePair<TKey, TElement>[] pairs = source.Select(s => KeyValuePair.Create(keySelector(s), elementSelector(s))).ToArray();
+        return new(pairs.Select(kvp => kvp.Value).ToImmutableArray(), pairs.ToFrozenDictionary(comparer));
+    }
+}
+/// <summary>
+/// An wrapper around a <seealso cref="FrozenDictionary"/> that also keeps a reference to the source array without affecting the original order.
+/// </summary>
+public class OrderedFrozenDictionary<TKey, TValue> where TKey : notnull
+{
+    internal OrderedFrozenDictionary(ImmutableArray<TValue> values, FrozenDictionary<TKey, TValue> dictionary) 
+    {
+        Values = values;
+        Dictionary = dictionary;
+    }
+    public ImmutableArray<TValue> Values { get; }
+    public FrozenDictionary<TKey, TValue> Dictionary { get; }
+}
---
+++ GameshowPro.Common/Model/OrderedSet.cs
@@ -0,0 +1,110 @@
+﻿
+using System.Runtime.Serialization;
+
+namespace GameshowPro.Common.Model;
+
+/// <summary>
+/// The best of lists and sets: order is maintained, while uniqueness is maintained and <see cref="Contains"/> is fast.
+/// </summary>
+/// <typeparam name="T">The type of elements in the list</typeparam>
+public class OrderedSet<T> : IList<T>, ICollection<T>, IEnumerable<T>, IEnumerable, IReadOnlyCollection<T>, ISet<T>, IReadOnlySet<T>, IDeserializationCallback, IReadOnlyList<T>, ICollection, IList
+{
+    private readonly List<T> _items = [];
+    private readonly HashSet<T> _itemsSet = [];
+
+    public T this[int index] { get => _items[index]; set => _items[index] = value; }
+
+    public int Count => _items.Count;
+
+    public bool IsReadOnly => false;
+
+    public bool IsSynchronized => ((ICollection)_items).IsSynchronized;
+
+    public object SyncRoot => ((ICollection)_items).SyncRoot;
+
+    public bool IsFixedSize => ((IList)_items).IsFixedSize;
+
+    object? IList.this[int index] { get => ((IList)_items)[index]; set => ((IList)_items)[index] = value; }
+
+    void ICollection<T>.Add(T item)
+        => _ = Add(item);
+
+    public bool Add(T item)
+    {
+        if (_itemsSet.Add(item))
+        {
+            _items.Add(item);
+            return true;
+        }
+        return false;
+    }
+
+    public void Clear()
+    {
+        _items.Clear();
+        _itemsSet.Clear();
+    }
+
+    public bool Contains(T item) => _itemsSet.Contains(item);
+
+    public void CopyTo(T[] array, int arrayIndex) => _items.CopyTo(array, arrayIndex);
+
+    public void ExceptWith(IEnumerable<T> other) => _itemsSet.ExceptWith(other);
+
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+
+    public int IndexOf(T item) => _items.IndexOf(item);
+
+    public void Insert(int index, T item) => _items.Insert(index, item);
+
+    public void IntersectWith(IEnumerable<T> other) => _itemsSet.IntersectWith(other);
+
+    public bool IsProperSubsetOf(IEnumerable<T> other) => _itemsSet.IsProperSubsetOf(other);
+
+    public bool IsProperSupersetOf(IEnumerable<T> other) => _itemsSet.IsProperSupersetOf(other);
+
+    public bool IsSubsetOf(IEnumerable<T> other) => _itemsSet.IsSubsetOf(other);
+
+    public bool IsSupersetOf(IEnumerable<T> other) => IsSupersetOf(other);
+
+    public bool Overlaps(IEnumerable<T> other) => _itemsSet.Overlaps(other);
+
+    public bool Remove(T item)
+    {
+        if (_itemsSet.Remove(item))
+        {
+            _items.Remove(item);
+            return true;
+        }
+        return false;
+    }
+
+    public void RemoveAt(int index)
+    {
+        T item = _items[index];
+        _items.RemoveAt(index);
+        _itemsSet.Remove(item);
+    }
+
+    public bool SetEquals(IEnumerable<T> other) => _itemsSet.SetEquals(other);
+
+    public void SymmetricExceptWith(IEnumerable<T> other) => _itemsSet.SymmetricExceptWith(other);
+
+    public void UnionWith(IEnumerable<T> other) => _itemsSet.UnionWith(other);
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public void OnDeserialization(object? sender) => _itemsSet.OnDeserialization(sender);
+
+    public void CopyTo(Array array, int index) => ((ICollection)_items).CopyTo(array, index);
+
+    public int Add(object? value) => ((IList)_items).Add(value);
+
+    public bool Contains(object? value) =>  ((IList)_items).Contains(value);
+
+    public int IndexOf(object? value) => ((IList)_items).IndexOf(value);
+
+    public void Insert(int index, object? value) => ((IList)_items).Insert(index, value);
+
+    public void Remove(object? value) => ((IList)_items).Remove(value);
+}
