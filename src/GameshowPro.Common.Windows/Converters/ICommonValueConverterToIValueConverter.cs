46d041a82fa07ca32c54ad861a46855fae84770f
Hamish Barjonas <github@barjonas.com>
2024-08-26 12:51:56 -0700

    Add StringFormatConverter

--- src/GameshowPro.Common.Windows/Converters/ICommonValueConverterToIValueConverter.cs
+++ src/GameshowPro.Common.Windows/Converters/ICommonValueConverterToIValueConverter.cs
@@ -45,3 +45,4 @@ public class ToIntConverter : BaseConverters.ToIntConverter, IValueConverter { }
 
 public class UpperCaseConverter : BaseConverters.UpperCaseConverter, IValueConverter { }
 public class PathPartConverter : BaseConverters.PathPartConverter, IValueConverter { }
+public class StringFormatConverter : BaseConverters.StringFormatConverter, IMultiValueConverter { }
---
+++ src/GameshowPro.Common/BaseConverters/StringFormatConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace GameshowPro.Common.BaseConverters;
+
+/// <summary>
+/// Apply a data-bindable format string to a number.
+/// </summary>
+public class StringFormatConverter : ICommonMultiValueConverter
+{
+    public object? Convert(object?[] values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values.Length < 2 || values[1] is not string format)
+        {
+            return values[0]?.ToString();
+        }
+        return string.Format($"{{0:{format}}}", values[0]);
+    }
+
+    public object?[] ConvertBack(object? value, Type[] targetTypes, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
