1cf12963146b86622693a0dbb0404e9def5bdcdc
Hamish Barjonas <github@barjonas.com>
2024-08-26 13:48:26 -0700

    Clean up GreaterThanToBooleanConverter

--- src/GameshowPro.Common.Windows/Converters/GreaterThanToBooleanConverter.cs
+++ src/GameshowPro.Common.Windows/Converters/GreaterThanToBooleanConverter.cs
@@ -18,10 +18,25 @@ public class GreaterThanToBooleanConverter : IValueConverter, IMultiValueConvert
         return BooleanToType(false, targetType);
     }
 
-    public object Convert(object value, Type targetType, object parameter,
-        CultureInfo culture)
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        return BooleanToType((double)value > (double)parameter, targetType);
+        if (value is double dValue && parameter is double dParam)
+        {
+            return BooleanToType(dValue > dParam, targetType);
+        }
+        if (value is int iValue && parameter is int iParam)
+        {
+            return BooleanToType(iValue > iParam, targetType);
+        }
+        if (value is long lValue && parameter is long lParam)
+        {
+            return BooleanToType(lValue > lParam, targetType);
+        }
+        if (value is float fValue && parameter is float fParam)
+        {
+            return BooleanToType(fValue > fParam, targetType);
+        }
+        throw new InvalidOperationException("Invalid combination of parameter and value types");
     }
 
     private object BooleanToType(bool value, Type type)
