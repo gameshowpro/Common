7332373e28647118c02a8362341f413558f7df0f
Hamish Barjonas <github@barjonas.com>
2025-02-14 22:04:32 -0800

    Add mDNS application conflict detection

---
+++ src/GameshowPro.Common/Model/IMdnsConflictMonitor.cs
@@ -0,0 +1,19 @@
+﻿namespace GameshowPro.Common.Model;
+
+/// <summary>
+/// Implementors use MDNS to maintain a list of other MDNS services with the same type as the current instance.
+/// This allows an interface to UIs to bring this to the attention of users.
+/// </summary>
+public interface IMdnsConflictMonitor
+{
+    ImmutableArray<IMdnsConflictingService> ConflictingServices { get; }
+}
+
+/// <summary>
+/// A single host on which a conflicting service was found, along with a list of advertised IP addresses.
+/// </summary>
+public interface IMdnsConflictingService
+{
+    string HostName { get; }
+    ImmutableArray<IPAddress> Addresses { get; }
+}
