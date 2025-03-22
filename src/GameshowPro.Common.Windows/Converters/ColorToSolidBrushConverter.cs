c8aa0b3290a6030ebc8626bdfba35b4060378007
Hamish Barjonas <github@barjonas.com>
2025-03-21 17:45:39 -0700

    Add color to solid brush converter

---
+++ src/GameshowPro.Common.Windows/Converters/ColorToSolidBrushConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace GameshowPro.Common.Converters;
+
+public class ColorToSolidBrushConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => new System.Windows.Media.SolidColorBrush((System.Windows.Media.Color)value);
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
